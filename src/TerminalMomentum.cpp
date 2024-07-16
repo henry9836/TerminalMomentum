c472d9cd8bdf4262ed9cf1eb0facd9e663dd87db
henry9836 <henry983615@gmail.com>
2024-07-16 19:21:50 +1200

    Implement FTXUI

--- src/TerminalMomentum.cpp
+++ src/TerminalMomentum.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
 #include "calc-example.h"
 
 using namespace std;
+using namespace ftxui;
 
 int main()
 {
@@ -9,5 +12,22 @@ int main()
 
     cout << "Testing 1 + 3: " << Calc::add(1, 3) << endl;
 
+    // Define the document
+    Element document =
+        hbox({
+        text("left")   | border,
+        text("this is a ftxui test :3") | border | flex,
+        text("right")  | border,
+        });
+    
+    auto screen = Screen::Create(
+        Dimension::Full(),       // Width
+        Dimension::Fit(document) // Height
+    );
+    Render(screen, document);
+    screen.Print();
+
     return 0;
 }
+
+
