5b0cb85ad83a3ef3a8cc30b8992728da796c9050
Henry Oliver <henry983615@gmail.com>
2024-07-21 00:09:32 +1200

    Testing the stuffs

--- src/TerminalMomentum.cpp
+++ src/TerminalMomentum.cpp
@@ -1,31 +1,60 @@
 #include <iostream>
-#include <ftxui/dom/elements.hpp>
+#include <fstream>
 #include <ftxui/screen/screen.hpp>
 #include "calc-example.h"
+
+ 
+#include "ftxui/component/captured_mouse.hpp"  // for ftxui
+#include "ftxui/component/component.hpp"       // for Input, Renderer, Vertical
+#include "ftxui/component/component_base.hpp"  // for ComponentBase
+#include "ftxui/component/component_options.hpp"  // for InputOption
+#include "ftxui/component/screen_interactive.hpp"  // for Component, ScreenInteractive
+#include "ftxui/dom/elements.hpp"  // for text, hbox, separator, Element, operator|, vbox, border
+#include "ftxui/util/ref.hpp"  // for Ref
 
 using namespace std;
 using namespace ftxui;
 
 int main()
 {
-    printf("hi\n");
+    string InputTest;
 
-    cout << "Testing 1 + 3: " << Calc::add(1, 3) << endl;
+    Component InputTestComponent = Input(&InputTest, "Search");
 
-    // Define the document
-    Element document =
-        hbox({
-        text("left")   | border,
-        text("this is a ftxui test :3") | border | flex,
-        text("right")  | border,
-        });
+    // // Filter out non-digit characters.
+    // InputTestComponent |= CatchEvent([&](Event event) {
+    //     return event.is_character() && !std::isdigit(event.character()[0]);
+    // });
     
-    auto screen = Screen::Create(
-        Dimension::Full(),       // Width
-        Dimension::Fit(document) // Height
-    );
-    Render(screen, document);
-    screen.Print();
+    // Filter out returns
+    InputTestComponent |= CatchEvent([&](Event event) {
+        if (event == Event::Return)
+        {
+            ofstream outfile(InputTest);
+            outfile.close();
+            // Invoke a std::system call but have the editor used be whitelisted
+            // The name of the text file will also have to be santised
+            InputTest = "";
+        }
+        return (event == Event::Return);
+    });
+
+    // The component tree:
+    auto component = Container::Vertical({
+        InputTestComponent,
+    });
+    
+    // Tweak how the component tree is rendered:
+    auto renderer = Renderer(component, [&] {
+        return vbox({
+                hbox(InputTestComponent->Render()) | border,
+                text("Results") | border | flex,
+                text("Preview")  | border,
+        });
+    });
+    
+    auto screen = ScreenInteractive::TerminalOutput();
+    screen.Loop(renderer);
 
     return 0;
 }
